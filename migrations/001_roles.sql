f44d2585e23220b6032dd5c14c6869ec7718dbde
Evgeny Dobryansky <noeskize@gmail.com>
2026-08-04 06:00:19 +0300

    fix: make cluster role bootstrap idempotent

--- migrations/001_roles.sql
+++ migrations/001_roles.sql
@@ -1,7 +1,7 @@
 -- Run as a PostgreSQL cluster administrator. Credentials are supplied by deployment secrets.
-CREATE ROLE event_store_owner NOLOGIN;
-CREATE ROLE event_store_migrator LOGIN NOINHERIT;
-CREATE ROLE event_store_app LOGIN NOINHERIT;
-CREATE ROLE event_store_cdc LOGIN REPLICATION NOINHERIT;
-CREATE ROLE projection_worker LOGIN NOINHERIT;
+DO $$ BEGIN CREATE ROLE event_store_owner NOLOGIN; EXCEPTION WHEN duplicate_object THEN NULL; END $$;
+DO $$ BEGIN CREATE ROLE event_store_migrator LOGIN NOINHERIT; EXCEPTION WHEN duplicate_object THEN NULL; END $$;
+DO $$ BEGIN CREATE ROLE event_store_app LOGIN NOINHERIT; EXCEPTION WHEN duplicate_object THEN NULL; END $$;
+DO $$ BEGIN CREATE ROLE event_store_cdc LOGIN REPLICATION NOINHERIT; EXCEPTION WHEN duplicate_object THEN NULL; END $$;
+DO $$ BEGIN CREATE ROLE projection_worker LOGIN NOINHERIT; EXCEPTION WHEN duplicate_object THEN NULL; END $$;
 GRANT event_store_owner TO event_store_migrator;
